36adea24c1bb48e46ef88a7490afe1313e70a069
Anika Islam <anikaislamporoma17@gmail.com>
2024-10-07 01:27:48 +0600

    updated

--- course-tracker/vite.config.js
+++ course-tracker/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Portfolio/', // Set this to your repository name
+  base: '/BRACU_UG_CourseTracker/', // Set this to your repository name
   plugins: [react()],
 });
 
