88e9f4a4e7e1298b3a9eb1cb824282e1fc5f8df1
Anika Islam <anikaislamporoma17@gmail.com>
2024-10-17 19:07:44 +0600

    git account added in footer

--- course-tracker/src/components/Footer.jsx
+++ course-tracker/src/components/Footer.jsx
@@ -4,8 +4,17 @@ const Footer = () => {
   return (
     <div className="flex justify-center bottom-7">
         <div className="text-coffee-200 text-center mb-10 font-mono ">
-        by anikabytes 
-        <p className="text-coffee-200 ">Version 1.0</p>
+        © October, 2024 |     
+        <a
+        href={"https://github.com/anikabytes"}
+        target="_blank"
+        rel="noopener noreferrer"
+        className = "hover:text-orange-200 ">
+          | by anikabytes 
+        </a>
+        
+
+        {/* <p className="text-coffee-200 ">Version 1.0</p> */}
         </div>
     </div>
   )
